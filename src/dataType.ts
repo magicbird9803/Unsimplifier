34176c81589ebe78ed13699b10a266e6c239e40d
magicbird9803 <105740171+magicbird9803@users.noreply.github.com>
2026-06-18 00:24:31 +0300

    Add BattleUnitParts data types to enum

---
+++ src/dataType.ts
@@ -0,0 +1,116 @@
+export enum DataType {
+	None,
+	
+	// map dispos
+	Npc,
+	Item,
+	Mobj,
+	Aobj,
+	Bshape,
+	GobjRes,
+	Effect,
+	MapParam,
+	Maplink,
+	
+	// registries
+	CharacterNpc,
+	CharacterMobj,
+	CharacterParty,
+	CharacterItem,
+	CharacterAobj,
+	
+	MapId,
+	ItemList,
+	MapItemLotTable,
+	EventFlag,
+
+	SndBattle,
+
+	//parameters
+	ParamActionBalloon,
+	ParamFade,
+	ParamField,
+	ParamHouseDoor,
+	ParamJump,
+	ParamPartyHint,
+	ParamPlayer,
+	ParamGobj,
+	ParamGobjItem,
+	DataMinigamePaperAiper,
+	DataMinigamePaperFan,
+	DataMinigamePaperRunner,
+	DataMinigamePaperRunnerai,
+
+	//battle
+	Monosiri,
+	FallObj,
+	Nozzle,
+	HeartParam,
+	Parameter,
+
+	BattleWeaponMario,
+	BattleWeaponParty,
+	BattleWeaponOther,
+	BattleWeaponEnemy,
+	BattleWeaponItem,
+	BattleWeaponAcMarioAc,
+	BattleWeaponAcPartyAc,
+
+	BattlePoseTable,
+	BattleAudienceKind,
+	BattleUnitParts,
+
+	// data_model.elf
+	BattlePosePoses,
+	BattlePoseTableType,
+	ModelType,
+	DataBattleModel,
+	DataGobjModel,
+	DataItemModel,
+	DataMobjModel,
+	DataNpcModel,
+	DataPlayerModel,
+
+	DataUi,
+
+	// sound
+	DataSnd,
+
+
+	// this is the end of the actual file types and start of sub types
+	TypeAmount,
+	ListItem,
+	HeartItem,
+
+	ModelAssetGroup,
+	ModelState,
+	ModelFaceGroup,
+	ModelFace,
+	ModelAnimation,
+
+	
+	MaplinkHeader,
+
+	SndBattleHeader,
+
+	UiModel,
+	UiModelProperty,
+	UiAcMaster,
+	UiGalleryArt,
+	UiGallerySound,
+	UiIcon,
+	UiMail,
+	UiMap,
+	UiMessage,
+	UiSelectWindow,
+	UiShine,
+	UiShopItem,
+	UiShop,
+	UiSellItem,
+	UiStarpiece,
+	UiStyle,
+	UiUranaisiNext,
+
+	BattleUnitPartsLookup,
+	BattleUnitPart,
+}
